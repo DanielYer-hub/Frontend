95749be279f16769ab1e1e47cb3490123dc8793b
DanielYer-hub <komrad.erema@yandex.com>
2025-12-08 22:11:03 +0200

    upd

--- generic-your-way/src/services/userService.ts
+++ generic-your-way/src/services/userService.ts
@@ -2,40 +2,34 @@ import { api } from "./http";
 import type { User } from "../interface/user/User";
 
 export async function registerUser(normalizedUser: User) {
-  const { data } = await api.post(
-    "http://localhost:8181/api/auth/register", 
-    normalizedUser
-  );
+  const { data } = await api.post("/auth/register", normalizedUser);
   return data;
 }
 
 export async function loginUser(email: string, password: string) {
-  const { data } = await api.post(
-    "http://localhost:8181/api/auth/login",    
-    { email, password }
-  );
+  const { data } = await api.post("/auth/login", { email, password });
   return data;
 }
 
 export async function getUserById(id: string) {
-  const { data } = await api.get(`/${id}`);
+  const { data } = await api.get(`/users/${id}`);
   return data;
 }
 
 export async function updateMe(patch: any) {
-  const { data } = await api.patch("http://localhost:8181/api/users/me", patch);
-  return data.user; 
+  const { data } = await api.patch("/users/me", patch);
+  return data.user;
 }
 
 export async function getMe() {
-  const { data } = await api.get("http://localhost:8181/api/users/me");
+  const { data } = await api.get("/users/me");
   return data.user;
 }
 
 export async function uploadMyPhoto(file: File) {
   const fd = new FormData();
   fd.append("photo", file);
-  const { data } = await api.post("/api/users/me/photo", fd, {
+  const { data } = await api.post("/users/me/photo", fd, {
     headers: { "Content-Type": "multipart/form-data" },
   });
   return data.user;
